4d928de1e14d0e1ee03ff3ba4a63d4b4ec76f7cb
yokn <discolouredattic@gmail.com>
2020-07-29 14:36:09 +0300

    initial commit

---
+++ node.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Node
+  def initialize(value, left, right)
+    @value = value
+    @left = left
+    @right = right
+  end
+end
---
+++ tree.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Tree
+  def initialize
+    @root = nil
+  end
+
+  def build_tree(arr); end
+end
+
+my_tree = Tree.new
+my_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
