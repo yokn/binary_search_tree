57451881d29204a65cc0abb88221f168e717e31d
yokn <discolouredattic@gmail.com>
2020-08-02 12:30:47 +0300

    need to find a way to get top root

--- tree.rb
+++ tree.rb
@@ -7,31 +7,46 @@ class Tree
     @sorted = false
   end
 
+  # rubocop:disable Metrics/MethodLength
   def build_tree(arr, first = 0, last = nil)
-    puts '-------------------------------------------------------------------'
+    # puts '-------------------------------------------------------------------'
     unless @sorted
       arr.uniq!.sort!
       @sorted = true
     end
-    p arr
+    # p arr
     last ||= arr.length - 1
-    p "first is: #{first}"
-    p "last is: #{last}"
+    # p "first is: #{first}"
+    # p "last is: #{last}"
     return nil if first > last
 
     mid = (first + last) / 2
-    p "mid is: #{mid}"
+    # p "mid is: #{mid}"
 
-    p root = Node.new(arr[mid])
+    root = Node.new(arr[mid])
 
-    p 'building left'
+    # p 'building left'
     root.left = build_tree(arr, first, mid - 1)
-    p "left is now: #{root.left}"
-    p 'building right'
+    # p "left is now: #{root.left}"
+    # p 'building right'
     root.right = build_tree(arr, mid + 1, last)
-    p "right is now: #{root.right}"
-
+    # p "right is now: #{root.right}"
     root
+  end
+
+  def insert(number)
+    p number
+    p root
+    result = number <=> root
+    p result
+    case result
+    when 1
+      p '1'
+    when 0
+      p '0'
+    when -1
+      p '-1'
+    end
   end
 end
 
@@ -43,3 +58,4 @@ end
 
 my_tree = Tree.new
 pretty_print(my_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]))
+my_tree.insert(2)
